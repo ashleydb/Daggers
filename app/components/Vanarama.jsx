926651146c50fad1ea67571a1d11aed02ffa3996
Ashley Bennett <ashleydb@users.noreply.github.com>
2022-02-26 14:28:54 -0800

    Remove Vanarama form, upgrade Yarn

--- app/components/Vanarama.jsx
+++ app/components/Vanarama.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Page from 'Page';
-import HubspotForm from 'react-hubspot-form'
+//import HubspotForm from 'react-hubspot-form'
 
 // TODO: All content on this page.
 
@@ -14,6 +14,30 @@ export default class Class extends React.Component {
     componentDidMount() {
         ReactDOM.findDOMNode(this._contentTop).scrollIntoView();
     }
+    // render() {
+    //     return (
+    //         <div>
+    //             <div id="contentTop" name="contentTop" ref={(ref) => this._contentTop = ref} />
+    //             <div className="row">
+    //                 <div className="columns small-12">
+
+    //                     <Page pageId='Vanarama' />
+
+    //                 </div>
+    //             </div>
+    //             <div className="row">
+    //                 <div className="columns small-12">
+
+    //                     <HubspotForm
+    //                         portalId='4630307'
+    //                         formId='db2fccad-65ee-415f-831d-5ab974b081d5'
+    //                     />
+
+    //                 </div>
+    //             </div>
+    //         </div>
+    //     );
+    // }
     render() {
         return (
             <div>
@@ -22,16 +46,6 @@ export default class Class extends React.Component {
                     <div className="columns small-12">
 
                         <Page pageId='Vanarama' />
-
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="columns small-12">
-
-                        <HubspotForm
-                            portalId='4630307'
-                            formId='db2fccad-65ee-415f-831d-5ab974b081d5'
-                        />
 
                     </div>
                 </div>
